96f3e9b7526e8d2ec3fb64b34fefe5a78df7a12b
jiatang1603-byte <jiatang1603@gmail.com>
2026-06-10 15:11:58 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/google-sheets/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
